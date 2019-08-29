9d7dff71c7b049fc731930a259813f22674a2240
otumian <popecan1000@gmail.com>
2019-08-29 13:42:51 +0000

    modified all the files

--- Animals/Birds.py
+++ Animals/Birds.py
@@ -2,9 +2,9 @@ class Birds:
     def __init__(self):
         ''' Constructor for this class. '''
         # Create some member animals
-        self.members = ['Sparrow', 'Robin', 'Duck']
- 
- 
+        self.members = ['Sparrow', 'An actual flying ponny', 'Duck']
+
+
     def printMembers(self):
         print('Printing members of the Birds class')
         for member in self.members:
